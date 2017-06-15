4db4380f23b4c358ddb336a49d8f1d032dc11e9c
Bo Chen <bo.chen@kindredgroup.com>
2017-06-15 16:53:50 +1000

    if you are using import syntax in your entry point, you must use import "babel-polyfill" instead of require

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
+// append below line before graphQLServer.use('/'...)
+//graphQLServer.use(_express2.default.static('./public'));  //iis-node set current directory to 'azure'
+// require('babel-polyfill'); doesn't work!
+//If you are using ES6’s import syntax in your application’s entry point, you should instead import the polyfill at the top of the entry point to ensure the polyfills are loaded first
+import 'babel-polyfill';
 import express from 'express';
 import bodyParser from 'body-parser';
 import graphQLHTTP from 'express-graphql';
 import {schema} from './data/schema';
 import IO from 'socket.io';
 import subscriptionHandler from './server/subscriptionHandler';
-//*****************************************************************************************/
-
 //if you don't have below line, nodemon won't re-load generateSchemaJson, 
 //and as such your old schema.json is used
 import './scripts/updateSchema';
+//******************************* end import  *************************************************/
+
+
 //however you don't have to explicitly call the generateSchemaJSON method, as it's automatically 
 //callel everytime the file is loaded (node loads that module)
 const GRAPHQL_PORT = process.env.port || 8081;
-
 // Expose a GraphQL endpoint
 const graphQLServer = express();
 graphQLServer.use(bodyParser.json());
