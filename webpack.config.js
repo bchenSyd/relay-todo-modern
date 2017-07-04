df6b561f638c148c40e95a5d57a9879f09994fe4
Bo Chen <bo.chen@kindredgroup.com>
2017-07-04 14:23:13 +1000

    prevent webpack from generating source-map files for node_modules/* except for react-relay and relay-runtime

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,23 @@
 const path = require('path');
 const webpack = require('webpack');
 
+const isVendorModule = (module) => {
+  const white_list = ['react-relay','relay-runtime'];
+  if(module.context){
+    if(white_list.find(m=> module.context.indexOf(m) !== -1 )){
+      return false; // don't put it into vendor.js
+    }
+    return module.context.indexOf('node_modules') !== -1;
+  }
+  return false;
+}
+
 var config = {
   debug: true,
-  devtool: 'source-map',
-  entry: ['whatwg-fetch', path.resolve(__dirname, 'js', 'app.js')],
+  entry: {
+    vendor: ['whatwg-fetch'],
+    a: path.resolve(__dirname, 'js', 'app.js'),
+  },
   output: {
     // easy to understand. where do you want to store the linked [filename] at server side? (client needs to download bundle.js first from server)
     // you will only notice this when you do a build (wds store files in memory)
@@ -19,12 +32,27 @@ var config = {
     // e.g. css-loader needs to handle the `url("imag/greentick.png")`, but where to fetch the png file?
     // the author of `css-loader` may refer to webpack.config.publicPath to figure it out if he/she likes (most don't, which render this configuration meaningless)
     // `wds` also take a hint from it, which is where the [virtual directory] thing comes from
-    filename: 'bundle.js',
+    filename: '[name].js',
   },
   plugins: [
     new webpack.DefinePlugin({
       //PRODUCTION: JSON.stringify(true), must have JSON.stringify !!
       '__WSENV__': JSON.stringify('http://localhost:8081'),
+    }),
+
+    /** you don't want to generate source map for vendor code; 8M -> 1M
+     * https://github.com/webpack/webpack/issues/2431#issuecomment-293281981
+     */
+    // Seperate vendor code into a seperate file
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      minChunks: isVendorModule
+    }),
+    new webpack.SourceMapDevToolPlugin({
+      filename: '[file].map',
+      exclude: [
+        'vendor.js',
+      ]
     }),
   ],
   module: {
