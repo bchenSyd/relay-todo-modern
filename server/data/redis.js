36195693addaf569c8613ca4131dbb1945ed3502
Bo Chen <bo.chen@westpac.com.au>
2018-07-26 15:36:21 +1000

    hmset and rpush are both synchronous functions

--- server/data/redis.js
+++ server/data/redis.js
@@ -6,20 +6,46 @@ let redis_client, pLRange; // passed in from server/index.js when server is star
 let races_topic = 'races';
 const setClient = async client => {
   const pFlushAll = promisify(client.flushall).bind(client);
-  debugger;
   await pFlushAll();
   redis_client = client;
 };
 const initRaces = async client => {
   await setClient(client);
-  Array.from({ length: 5 }, (val, index) => {
+  Array.from({ length: 5 }, async (val, index) => {
     const race_id = `R:${index}`;
-    redis_client.hmset(race_id, {
+    const race = {
       id: race_id,
       text: `Race ${index}`,
       completed: false,
-    });
-    redis_client.rpush(races_topic, race_id);
+    };
+    await insertRace(race);
+  });
+};
+
+const insertRace = async race => {
+  const { id: raceId } = race;
+  //   console.log('---------------------------  hmset ', race, '           begin')
+  //   await pHMSet(raceId, race);
+  //   console.log('---------------------------  hmset ', race, '           completed')
+  //   await pRPush(races_topic, raceId);
+
+  redis_client.hmset(raceId, race); // this is a synchronous function;
+  redis_client.rpush(races_topic, raceId); // this is a synchronous function;
+};
+
+// advanced: use pMap
+const getRaces = () => {
+  if (!redis_client) {
+    throw new Error('redis_client not exists. Please connect to redis first');
+  }
+  const pLrange = promisify(redis_client.lrange).bind(redis_client);
+  const pHGetAll = promisify(redis_client.hgetall).bind(redis_client);
+
+  return pLrange(races_topic, 0, -1).then((races, error) => {
+    debugger;
+    if (!error) {
+      return pMap(races, r => pHGetAll(r));
+    }
   });
 };
 
@@ -36,18 +62,4 @@ const getRaces_Primitive_Ugly = () => {
     });
   });
 };
-
-// advanced: use pMap
-const getRaces = () => {
-  if (!redis_client) {
-    throw new Error('redis_client not exists. Please connect to redis first');
-  }
-  const pLrange = promisify(redis_client.lrange).bind(redis_client);
-  const pHGetAll = promisify(redis_client.hgetall).bind(redis_client);
-
-  return pLrange(races_topic, 0, -1).then((races, error) => {
-    return pMap(races, r => pHGetAll(r));
-  });
-};
-
-module.exports = { initRaces, getRaces };
+module.exports = { initRaces, insertRace, getRaces };
